e13d6a693cb438e1c9cdc97c52383796d8bad02c
Vikramjeet Singh <c-vikramjeet.singh@round.glass>
2019-11-30 13:36:03 -0800

    Added docs folder for deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
+const API_URL = 'https://api.github.com';
+const ORG_NAME = 'vmware';
+const CLIENT_ID = 'Iv1.df794e5155d0d7f7';
+const CLIENT_SECRET = '6843f9d87f509b50b9d31103e08016899e659e04';
+
 export const environment = {
-  production: true
+  production: true,
+  orgDetail: `${API_URL}/orgs/${ORG_NAME}?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+  listOfRepos: `${API_URL}/orgs/${ORG_NAME}/repos?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`,
+  listOfProjects: `${API_URL}/orgs/${ORG_NAME}/projects?client_id=${CLIENT_ID}&client_secret=${CLIENT_SECRET}`
 };
